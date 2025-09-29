e8ce945a529896132bbd716458a0529b6df66732
vsls <vasilisa_001@mail.ru>
2025-09-30 00:09:27 +0600

    KOROTAEVA-4 добавлен фрагмент с ComposeView

---
+++ app/src/main/java/ru/vsls/korotaevahomework/UsersListFragment.kt
@@ -0,0 +1,31 @@
+package ru.vsls.korotaevahomework
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+
+class UsersListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_users_list, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    Text("Hello, Compose!")
+                }
+            }
+        }
+        return view
+    }
+}
