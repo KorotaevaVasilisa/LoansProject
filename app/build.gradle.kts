d823661f95b90f13d20b6d3b812bafe029091aa6
vsls <vasilisa_001@mail.ru>
2025-10-02 23:14:27 +0600

    KOROTAEVA-4 зависимость для отображения preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.material3.android)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
