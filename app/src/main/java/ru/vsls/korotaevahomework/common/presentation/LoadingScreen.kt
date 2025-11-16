595dbe8251f47e090c38cdef3124ede8b634d9f3
vsls <vasilisa_001@mail.ru>
2025-11-16 20:02:42 +0600

    KOROTAEVA правки загрузочного экрана

--- app/src/main/java/ru/vsls/korotaevahomework/common/presentation/LoadingScreen.kt
+++ app/src/main/java/ru/vsls/korotaevahomework/common/presentation/LoadingScreen.kt
@@ -1,21 +1,29 @@
-package ru.vsls.korotaevahomework.enter.presentation.common
+package ru.vsls.korotaevahomework.common.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import ru.vsls.korotaevahomework.enter.presentation.components.LogoBlock
 
 @Composable
 fun LoadingScreen() {
     Box(
-        modifier = Modifier
+        modifier = Modifier.Companion
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.primaryContainer)
     ) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        Column(
+            modifier = Modifier.Companion.align(Alignment.Companion.Center),
+            horizontalAlignment = Alignment.Companion.CenterHorizontally
+        ) {
+            LogoBlock()
+            CircularProgressIndicator()
+        }
     }
 }
--- app/src/main/java/ru/vsls/korotaevahomework/enter/presentation/components/EnterContent.kt
+++ app/src/main/java/ru/vsls/korotaevahomework/enter/presentation/components/EnterContent.kt
@@ -54,7 +54,7 @@ internal fun EnterContent(
 }
 
 @Composable
-private fun LogoBlock(modifier: Modifier) {
+internal fun LogoBlock(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
     ) {
