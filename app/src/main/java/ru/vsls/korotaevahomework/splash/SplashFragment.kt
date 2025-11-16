c4bf594d9b19ac1a155e2d49e3985a4357519d7c
vsls <vasilisa_001@mail.ru>
2025-11-16 20:14:53 +0600

    KOROTAEVA запуск splash экрана

--- app/src/main/java/ru/vsls/korotaevahomework/splash/SplashFragment.kt
+++ app/src/main/java/ru/vsls/korotaevahomework/splash/SplashFragment.kt
@@ -1,38 +1,24 @@
-package ru.vsls.korotaevahomework.enter.presentation
+package ru.vsls.korotaevahomework.splash
 
-import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.Fragment
-import ru.vsls.korotaevahomework.App
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.vsls.korotaevahomework.R
 import ru.vsls.korotaevahomework.common.theme.ShiftTheme
-import javax.inject.Inject
-import kotlin.getValue
+import ru.vsls.korotaevahomework.enter.presentation.EnterScreen
 
-class EnterFragment : Fragment() {
-
-    @Inject
-    lateinit var viewModel: EnterViewModel
-    private val component by lazy {
-        (requireActivity().application as App).component
-    }
-
-    override fun onAttach(context: Context) {
-        component.inject(this)
-        super.onAttach(context)
-    }
+class SplashFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_enter, container, false)
-
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,7 +28,7 @@ class EnterFragment : Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 ShiftTheme {
-                    EnterScreen(viewModel = viewModel)
+                    SplashScreen()
                 }
             }
         }
---
+++ app/src/main/java/ru/vsls/korotaevahomework/splash/SplashScreen.kt
@@ -0,0 +1,8 @@
+package ru.vsls.korotaevahomework.splash
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+
+}
