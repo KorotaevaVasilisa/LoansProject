eb0770a4e7dcadd48efb5abc12afffd1d7c5db6b
vsls <vasilisa_001@mail.ru>
2025-11-20 21:46:50 +0600

    KOROTAEVA валидация полей

--- app/src/main/java/ru/vsls/korotaevahomework/form/FormFragment.kt
+++ app/src/main/java/ru/vsls/korotaevahomework/form/FormFragment.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputLayout
 import ru.vsls.korotaevahomework.App
 import ru.vsls.korotaevahomework.R
 import javax.inject.Inject
@@ -14,6 +20,10 @@ class FormFragment : Fragment() {
     private var period: Int? = null
     private var percent: Double? = null
     private var amount: Int? = null
+    private var nameText: TextView? = null
+    private var surnameText: TextView? = null
+    private var numberText: TextView? = null
+    private var button: Button? = null
 
     @Inject
     lateinit var viewModelFactory: FormViewModel.Factory
@@ -49,6 +59,84 @@ class FormFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        nameText = view.findViewById(R.id.ed_name)
+        surnameText = view.findViewById(R.id.ed_surname)
+        numberText = view.findViewById(R.id.ed_number)
+        val layoutName = view.findViewById<TextInputLayout>(R.id.fir_name)
+        val layoutSurname = view.findViewById<TextInputLayout>(R.id.fir_surname)
+        val layoutNumber = view.findViewById<TextInputLayout>(R.id.fir_number)
+
+        nameText?.addTextChangedListener {
+            validateRussianText(nameText?.text?.toString(), layoutName)
+        }
+
+        surnameText?.addTextChangedListener {
+            validateRussianText(surnameText?.text?.toString(), layoutSurname)
+        }
+
+        numberText?.addTextChangedListener {
+            validateNumberText(numberText?.text?.toString(), layoutNumber)
+        }
+
+        setupToolbar(view)
+
+        val button = view.findViewById<Button>(R.id.confirm_button)
+        button.setOnClickListener {
+            val isNameValid = validateRussianText(nameText?.text?.toString(), layoutName)
+            val isSurnameValid = validateRussianText(surnameText?.text?.toString(), layoutSurname)
+            val isNumberValid = validateNumberText(numberText?.text?.toString(), layoutNumber)
+
+            if (isNameValid && isSurnameValid && isNumberValid) {
+                Toast.makeText(requireContext(), "OK!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun validateNumberText(text: String?, textInputLayout: TextInputLayout): Boolean {
+        return when {
+            text.isNullOrEmpty() -> {
+                textInputLayout.error = getString(R.string.empty_field)
+                false
+            }
+
+            text.length != 11 -> {
+                textInputLayout.error = getString(R.string.non_valid_number)
+                false
+            }
+
+            else -> {
+                textInputLayout.error = null
+                true
+            }
+        }
+    }
+
+    fun validateRussianText(text: String?, textInputLayout: TextInputLayout): Boolean {
+        return when {
+            text.isNullOrEmpty() -> {
+                textInputLayout.error = getString(R.string.empty_field)
+                false
+            }
+
+            !text.matches(Regex("^[а-яА-ЯёЁ\\s-]+$")) -> {
+                textInputLayout.error = getString(R.string.non_valid_text)
+                false
+            }
+
+            else -> {
+                textInputLayout.error = null
+                true
+            }
+        }
+    }
+
+    private fun setupToolbar(view: View) {
+        val toolbar =
+            view.findViewById<MaterialToolbar>(R.id.toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 
     companion object {
