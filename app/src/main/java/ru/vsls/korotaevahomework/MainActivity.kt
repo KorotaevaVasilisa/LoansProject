dcb3d7a3f97e6569be161ab1f30891a58ef6085e
vsls <vasilisa_001@mail.ru>
2025-11-23 00:02:44 +0600

    KOROTAEVA слушатель для навигации = экран меню

--- app/src/main/java/ru/vsls/korotaevahomework/MainActivity.kt
+++ app/src/main/java/ru/vsls/korotaevahomework/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.vsls.korotaevahomework.databinding.ActivityMainBinding
 import ru.vsls.navigation.ActivityHolder
 import ru.vsls.navigation.Router
@@ -36,11 +37,34 @@ class MainActivity : AppCompatActivity() {
         }
 
         holder.attachActivity(this)
+
+        setListenerBottomBar()
+
         if (savedInstanceState == null) {
             router.navigateTo(Screen.EnterScreen)
         }
     }
 
+    private fun setListenerBottomBar(){
+        val bottom = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.nav_home -> {
+                    router.clearBackStackAndNavigate(Screen.MainScreen)
+                    true
+                }
+
+                R.id.nav_menu -> {
+//                    router.replaceFragment(Screen.EnterScreen)
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
---
+++ app/src/main/java/ru/vsls/korotaevahomework/menu/MenuFragment.kt
@@ -0,0 +1,36 @@
+package ru.vsls.korotaevahomework.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import ru.vsls.korotaevahomework.R
+import ru.vsls.ui.theme.ShiftTheme
+
+class MenuFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ShiftTheme {
+
+                }
+            }
+        }
+    }
+}
