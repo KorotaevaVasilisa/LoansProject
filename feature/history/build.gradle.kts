d704c9981fac55d0e02b953eaf7851a1783731a6
vsls <vasilisa_001@mail.ru>
2025-11-23 19:27:26 +0600

    KOROTAEVA зависимость для превью

--- feature/history/build.gradle.kts
+++ feature/history/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.material3.android)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
