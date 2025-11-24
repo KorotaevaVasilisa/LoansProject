e9ee65d2bd7d3a29bd8b2c9c119b2c35866ac20c
vsls <vasilisa_001@mail.ru>
2025-11-24 21:38:22 +0600

    KOROTAEVA модуль для экрана формы

--- feature/form/build.gradle.kts
+++ feature/form/build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "ru.vsls.korotaevahomework"
+    namespace = "ru.vsls.form"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "ru.vsls.korotaevahomework"
         minSdk = 28
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,12 +32,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
-    viewBinding {
-        enable = true
-    }
 }
 
 dependencies {
@@ -49,39 +39,18 @@ dependencies {
     implementation(project(":shared:navigation"))
     implementation(project(":shared:ui"))
 
-    implementation(project(":feature:enter"))
-    implementation(project(":feature:history"))
-    implementation(project(":feature:main"))
-    implementation(project(":feature:details"))
-    implementation(project(":feature:form"))
-
     implementation(project(":utils"))
 
     implementation(libs.retrofit)
     implementation(libs.okhttp)
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.retrofit.converter.kotlinx.serialization)
-    implementation(libs.logging.interceptor)
 
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
 
-    implementation(libs.androidx.security.crypto)
-
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.lifecycle.viewmodel.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.material3.android)
-    debugImplementation(libs.androidx.compose.ui.tooling)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
